9e9980f0aad0729fa5d9fdeb14da8bd3fa9489c0
diegowfc <diegofernas@gmail.com>
2025-05-26 21:05:49 -0300

    Centralizing the GET /api/products information into one route name

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -4,16 +4,22 @@ import random
 class EcommerceUser(HttpUser):
     host = "https://localhost:7064"
     wait_time = between(1, 3)
-    
+    total_pages = 1389
+
     def on_start(self):
-        self.client.verify = False  # <-- Ignora verificação de certificado
-    
+        self.client.verify = False
+
     @task(3)
     def list_products(self):
-        page = random.randint(1, 100)     
-        page_size = 36              
-        self.client.get("/api/Product", params={"page": page, "pageSize": page_size})
-        
+        page = random.randint(1, self.total_pages)
+        page_size = 36
+
+        self.client.get(
+            "/api/Product",
+            params={"page": page, "pageSize": page_size},
+            name="/api/Product"
+        )
+
     @task(2)
     def create_order(self):
         num_items = random.randint(2, 4)
@@ -21,18 +27,15 @@ class EcommerceUser(HttpUser):
         product_ids = list(range(10010, 60010))
         selected_products = random.choices(product_ids, k=num_items)
 
-
-
         items = []
-        for product_id in selected_products:
+        for pid in selected_products:
             items.append({
-                "productId": product_id,
-                "quantity": random.randint(1, 3) 
+                "productId": pid,
+                "quantity": random.randint(1, 3)
             })
 
-        self.client.post("/api/Order", json={
-            "items": items
-        })
-    
-        
-  
+        self.client.post(
+            "/api/Order",
+            json={"items": items},
+            name="/api/Order"
+        )
