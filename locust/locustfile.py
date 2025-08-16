4a0d6d4d0534e00d8a88a57b503185888f5999a8
diegowfc <diegofernas@gmail.com>
2025-08-16 14:27:01 -0300

    page_size at locust

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -5,8 +5,8 @@ import string
 class EcommerceUser(HttpUser):
     host = "http://127.0.0.1:8080"
     wait_time = between(1, 3)
-    total_pages = 1389
-
+    page_size = 36
+    
     def on_start(self):
         self.client.verify = False
         self.after_id = None
